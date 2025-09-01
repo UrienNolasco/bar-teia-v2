4b0a5f5860aaa1dd78211b9e32eb2bd51c47f629
Urien Nolasco <urien.dragon@gmail.com>
2025-09-01 10:01:08 -0300

    feat: add toast and sooner for notify

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { BottomNav } from "@/components/ui/BottomNav";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/sonner";
 
 import { Header } from "@/components/ui/Header";
 
@@ -31,13 +32,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Toaster />
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
         >
-          
           <Header />
           <main className="flex-1 pb-16">{children}</main>
           <BottomNav />
